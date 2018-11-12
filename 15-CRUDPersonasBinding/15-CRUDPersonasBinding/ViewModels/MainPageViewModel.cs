6436bdf15593492e66e025f569ee39587f1fa0fe
adeborja <adeborja@users.noreply.github.com>
2018-11-12 14:39:38 +0100

    12/11/2018
    Añadidas funciones para borrar persona y actualizar lista, falta guardar los datos cuando se edita una persona y crear una nueva persona. El boton guardar debe hacer ambos, asi que hay que mirar la ID de la persona a guardar, si es 0 se va a crear una nueva y si no, se va a editar.

--- 15-CRUDPersonasBinding/15-CRUDPersonasBinding/ViewModels/MainPageViewModel.cs
+++ 15-CRUDPersonasBinding/15-CRUDPersonasBinding/ViewModels/MainPageViewModel.cs
@@ -7,10 +7,11 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
 
 namespace _15_CRUDPersonasBinding_UI.ViewModels
 {
-    class MainPageViewModel : INotifyPropertyChanged
+    class MainPageViewModel : clsVMBase
     {
         #region propiedades privadas
 
@@ -19,7 +20,10 @@ namespace _15_CRUDPersonasBinding_UI.ViewModels
         private List<clsDepartamento> _listadoDepartamentos;
         private clsDepartamento _departamentoSeleccionado;
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        private DelegateCommand _eliminarCommand;
+        private DelegateCommand _actualizarListadoCommand;
+
+        //public event PropertyChangedEventHandler PropertyChanged;
 
         #endregion
 
@@ -48,7 +52,10 @@ namespace _15_CRUDPersonasBinding_UI.ViewModels
             set
             {
                 _personaSeleccionada = value;
-                OnPropertyChanged("personaSeleccionada");
+
+                //Llamamos a CanExecute de eliminar para que compruebe si habilita el comando
+                _eliminarCommand.RaiseCanExecuteChanged();
+                NotifyPropertyChanged("personaSeleccionada");
             }
         }
 
@@ -74,32 +81,140 @@ namespace _15_CRUDPersonasBinding_UI.ViewModels
             set
             {
                 _departamentoSeleccionado = value;
-                OnPropertyChanged("departamentoSeleccionado");
+                NotifyPropertyChanged("departamentoSeleccionado");
             }
+        }
+
+
+        public DelegateCommand eliminarCommand
+        {
+            get
+            {
+                _eliminarCommand = new DelegateCommand(eliminarCommand_Executed, eliminarCommand_CanExecute);
+                return _eliminarCommand;
+            }
+        }
+
+        public DelegateCommand actualizarListadoCommand
+        {
+            get
+            {
+                _actualizarListadoCommand = new DelegateCommand(actualizarListadoCommand_Executed);
+                return _actualizarListadoCommand;
+            }
+        }
+
+        private void actualizarListadoCommand_Executed()
+        {
+            //Actualizamos la lista de personas
+            clsListadoPersonas_BL listadoPersonas = new clsListadoPersonas_BL();
+
+            //Cargar el listado de personas
+            _listadoDePersonas = listadoPersonas.listadoCompletoPersonas_BL();
+            NotifyPropertyChanged("listadoDePersonas"); //propiedad en linea 30
+        }
+
+
+
+
+
+        /// <summary>
+        /// Funcion para eliminar una persona de la base de datos
+        /// </summary>
+        //private void eliminarCommand_Executed()
+        //{
+        //    //Instanciar un objeto de la manejadora de la capa BL
+        //    clsManejadoraPersona_BL manejadoraBL = new clsManejadoraPersona_BL();
+
+        //    try //Vamos a llamar a la base de datos
+        //    {
+        //        manejadoraBL.borrarPersonaPorID_BL(personaSeleccionada.idPersona);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        //TODO lanzar un dialogo con error
+        //    }
+        //}
+
+
+
+        private async void eliminarCommand_Executed()
+        {
+            int filasAfectadas = -1;
+            clsManejadoraPersona_BL manejadoraBL = new clsManejadoraPersona_BL();
+            ContentDialog confirmarBorrado = new ContentDialog();
+            ContentDialogResult resultado;
+
+            //Definir las propiedades del popup
+            confirmarBorrado.Title = "Eliminar";
+            confirmarBorrado.Content = "¿¡Estas seguro de querer borrar a esta persona!?";
+            confirmarBorrado.PrimaryButtonText = "Cancelar";
+            confirmarBorrado.SecondaryButtonText = "Aceptar";
+
+            resultado = await confirmarBorrado.ShowAsync();
+            if(resultado == ContentDialogResult.Secondary)
+            {
+                try
+                {
+                    filasAfectadas = manejadoraBL.borrarPersonaPorID_BL(personaSeleccionada.idPersona);
+
+                    //Actualizamos la lista de personas
+                    //clsListadoPersonas_BL listadoPersonas = new clsListadoPersonas_BL();
+
+                    //Cargar el listado de personas
+                    //_listadoDePersonas = listadoPersonas.listadoCompletoPersonas_BL();
+                    //NotifyPropertyChanged("listadoDePersonas"); //propiedad en linea 30
+                    actualizarListadoCommand_Executed();
+                }
+                catch(Exception e)
+                {
+                    //TODO lo mismo del anterior
+                }
+            }
+            
+        }
+
+
+        /// <summary>
+        /// Funcion que devuelve un logico para habilitar o deshabilitar los controles bindeados al comando eliminar
+        /// </summary>
+        /// <returns></returns>
+        private bool eliminarCommand_CanExecute()
+        {
+            bool sePuedeEliminar = false;
+
+            if(personaSeleccionada!=null)
+            {
+                sePuedeEliminar = true;
+            }
+
+            return sePuedeEliminar;
         }
 
         #endregion
 
-        protected void OnPropertyChanged(string name)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null)
-            {
-                handler(this, new PropertyChangedEventArgs(name));
-            }
-        }
-     
+        //Al implementar la clase clsVMBase no es necesario utilizar INotifytaltal
+        //protected void OnPropertyChanged(string name)
+        //{
+        //    PropertyChangedEventHandler handler = PropertyChanged;
+        //    if (handler != null)
+        //    {
+        //        handler(this, new PropertyChangedEventArgs(name));
+        //    }
+        //}
+
 
 
         #region constructor por defecto
 
         public MainPageViewModel()
         {
-            clsListadoPersonas_BL listadoPersonas = new clsListadoPersonas_BL();
+            //clsListadoPersonas_BL listadoPersonas = new clsListadoPersonas_BL();
             clsListadoDepartamentos_BL listadoDepartamentos = new clsListadoDepartamentos_BL();
 
             //Cargar el listado de personas
-            _listadoDePersonas = listadoPersonas.listadoCompletoPersonas_BL();
+            //_listadoDePersonas = listadoPersonas.listadoCompletoPersonas_BL();
+            actualizarListadoCommand_Executed();
 
             _listadoDepartamentos = listadoDepartamentos.listadoCompletoPersonas_BL();
         }
