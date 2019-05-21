375c7aec6e429d272a61389e11d97e95a0ebc09f
Angel David de Borja <angeldaviddeborjagarcia@gmail.com>
2019-05-21 21:17:05 +0200

    sqlite

---
+++ 22-LeerGuardarArchivos-UWP/22-LeerGuardarArchivos-UWP/Models/clsPersona.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Soap;
+//using System.Runtime.Serialization.Formatters.Binary;
+
+namespace _15_CRUDPersonasBinding_Entidades
+{
+    //https://docs.microsoft.com/en-us/dotnet/api/system.serializableattribute?view=netframework-4.8
+    [Serializable()]
+    public class clsPersona
+    {
+        #region constructor por defecto
+
+        public clsPersona()
+        {
+            
+        }
+
+        #endregion
+
+        #region constructor por parametros
+
+        public clsPersona(String nNombre, String nApellido)
+        {
+            this.nombre = nNombre;
+            this.apellidos = nApellido;
+        }
+
+        #endregion
+
+
+        #region atributos y propiedades
+        
+        public String nombre { get; set; }
+        public String apellidos { get; set; }
+
+
+        #endregion
+    }
+}
---
+++ 23-SQLite-UWP/DataAccessLibrary/DataAccess.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Data.Sqlite;
+
+namespace DataAccessLibrary
+{
+    public static class DataAccess
+    {
+        public static void inicializarBaseDatos()
+        {
+            using (SqliteConnection db = new SqliteConnection("Filename=SQLitePrueba.db"))
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT EXISTS MyTable (Primary_Key INTEGER PRIMARY KEY, Text_Entry NVARCHAR(2048) NULL)";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+
+        public static void AddDatos(string inputText)
+        {
+            using (SqliteConnection db = new SqliteConnection("Filename=SQLitePrueba.db"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+
+                insertCommand.Connection = db;
+
+                //Usar sentencia preparada para evitar ataques de inyeccion SQL
+                insertCommand.CommandText = "INSERT INTO MyTable VALUES (NULL, @Entry);";
+                insertCommand.Parameters.AddWithValue("@Entry", inputText);
+
+                insertCommand.ExecuteReader();
+
+                db.Close();
+            }
+        }
+
+        public static List<String> getDatos()
+        {
+            List<String> textos = new List<string>();
+
+            using (SqliteConnection db = new SqliteConnection("Filename=SQLitePrueba.db"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand("SELECT Text_Entry FROM MyTable", db);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                while(query.Read())
+                {
+                    textos.Add(query.GetString(0));
+                }
+
+                db.Close();
+            }
+
+                return textos;
+        }
+    }
+}
